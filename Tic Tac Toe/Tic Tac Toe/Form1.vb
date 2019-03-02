b5a62c748a07f32acc805cd1c8aad98e76f46944
Debayan Dam <37379905+debayandam@users.noreply.github.com>
2019-03-02 23:42:25 +0530

    A Simple Tic-Tac-Toe game using Windows Form

---
+++ Tic Tac Toe/Tic Tac Toe/Form1.vb
@@ -0,0 +1,398 @@
+﻿Public Class Form1
+        Private Sub chektext1()
+            If Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button1.Text = "X" And Button2.Text = "X" And Button3.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X" Then
+                MsgBox("Congratulation! Player 1 Won")
+            Label3.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+            End If
+        End Sub
+
+        Private Sub checktext2()
+            If Button1.Text = "O" And Button5.Text = "O" And Button9.Text = "O" Then
+                MsgBox("Congratulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button1.Text = "O" And Button2.Text = "O" And Button3.Text = "O" Then
+                MsgBox("Congratulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button4.Text = "O" And Button5.Text = "O" And Button6.Text = "O" Then
+                MsgBox("Congratulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button7.Text = "O" And Button8.Text = "O" And Button9.Text = "O" Then
+                MsgBox("Congragulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button1.Text = "O" And Button4.Text = "O" And Button7.Text = "O" Then
+                MsgBox("Congragulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button2.Text = "O" And Button5.Text = "O" And Button8.Text = "O" Then
+                MsgBox("Congragulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button3.Text = "O" And Button6.Text = "O" And Button9.Text = "O" Then
+                MsgBox("Congragulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+
+            ElseIf Button3.Text = "O" And Button5.Text = "O" And Button7.Text = "O" Then
+                MsgBox("Congragulation! Player 2 Won")
+            Label4.Text += 1
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Button3.Enabled = False
+                Button4.Enabled = False
+                Button5.Enabled = False
+                Button6.Enabled = False
+                Button7.Enabled = False
+                Button8.Enabled = False
+                Button9.Enabled = False
+            End If
+        End Sub
+
+        Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+            If Label2.Visible = True Then
+                Button1.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button1.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button1.Enabled = False
+        End Sub
+
+        Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+            If Label2.Visible = True Then
+                Button2.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button2.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button2.Enabled = False
+        End Sub
+
+        Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+            If Label2.Visible = True Then
+                Button3.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button3.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button3.Enabled = False
+        End Sub
+
+        Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+            If Label2.Visible = True Then
+                Button4.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button4.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button4.Enabled = False
+        End Sub
+
+        Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+            If Label2.Visible = True Then
+                Button5.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button5.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button5.Enabled = False
+        End Sub
+
+        Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+            If Label2.Visible = True Then
+                Button6.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button6.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button6.Enabled = False
+        End Sub
+
+        Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+            If Label2.Visible = True Then
+                Button7.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button7.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button7.Enabled = False
+        End Sub
+
+        Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+            If Label2.Visible = True Then
+                Button8.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button8.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button8.Enabled = False
+        End Sub
+
+        Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+            If Label2.Visible = True Then
+                Button9.Text = "O"
+                checktext2()
+                Label2.Visible = False
+                Label1.Visible = True
+                GoTo 1000
+            End If
+            If Label1.Visible = True Then
+                Button9.Text = "X"
+                chektext1()
+                Label1.Visible = False
+                Label2.Visible = True
+                GoTo 1000
+            End If
+1000:       Button9.Enabled = False
+        End Sub
+
+        Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+            Button1.Text = ""
+            Button1.Enabled = True
+            Button2.Text = ""
+            Button2.Enabled = True
+            Button3.Text = ""
+            Button3.Enabled = True
+            Button4.Text = ""
+            Button4.Enabled = True
+            Button5.Text = ""
+            Button5.Enabled = True
+            Button6.Text = ""
+            Button6.Enabled = True
+            Button7.Text = ""
+            Button7.Enabled = True
+            Button8.Text = ""
+            Button8.Enabled = True
+            Button9.Text = ""
+            Button9.Enabled = True
+        End Sub
+    End Class
